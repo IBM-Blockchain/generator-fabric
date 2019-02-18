387e632739daf6a5cd8f16de7cf473c501633e5c
Simon Stone <sstone1@uk.ibm.com>
2019-02-18 14:44:54 +0000

    Enable MacOS and Windows builds, add automatic deploy (#61)
    Signed-off-by: Simon Stone <sstone1@uk.ibm.com>

--- generators/chaincode/templates/javascript/.eslintrc.js
+++ generators/chaincode/templates/javascript/.eslintrc.js
@@ -14,7 +14,6 @@ module.exports = {
     extends: "eslint:recommended",
     rules: {
         indent: ['error', 4],
-        'linebreak-style': ['error', 'unix'],
         quotes: ['error', 'single'],
         semi: ['error', 'always'],
         'no-unused-vars': ['error', { args: 'none' }],
