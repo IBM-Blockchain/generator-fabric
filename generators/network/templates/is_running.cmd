7e375acd9cb6f0636ecc3de1bbbacc654e6e14f6
Caroline Fletcher <caroline.fletcher@uk.ibm.com>
2019-06-17 13:10:53 +0100

    stuff
    Signed-off-by: Caroline Fletcher <caroline.fletcher@uk.ibm.com>

--- generators/network/templates/is_running.cmd
+++ generators/network/templates/is_running.cmd
@@ -5,13 +5,21 @@ rem
 rem SPDX-License-Identifier: Apache-2.0
 rem
 setlocal enabledelayedexpansion
-for %%c in (<%= dockerName %>_orderer.example.com <%= dockerName %>_ca.org1.example.com <%= dockerName %>_peer0.org1.example.com <%= dockerName %>_couchdb <%= dockerName %>_logspout) do (
-  for /f "usebackq delims=" %%i in (`docker inspect -f {{.State.Running}} %%c 2^>:NUL`) do set running=%%i
+
+IF [%1] == [] (
+    CALL :isRunning <%= dockerName %>_peer0.org1.example.com
+) ELSE (
+    for %%c in (<%= dockerName %>_orderer.example.com <%= dockerName %>_ca.org1.example.com <%= dockerName %>_peer0.org1.example.com <%= dockerName %>_couchdb <%= dockerName %>_logspout) do (
+        CALL :isRunning %%i
+    )
+)
+exit /b 0
+
+:isRunning
+  for /f "usebackq delims=" %~1 in (`docker inspect -f {{.State.Running}} %%c 2^>:NUL`) do set running=%~i
   if not !errorlevel! == 0 (
     exit /b 1
   )
   if not "!running!" == "true" (
     exit /b 1
   )
-)
-exit /b 0
--- generators/network/templates/is_running.sh
+++ generators/network/templates/is_running.sh
@@ -6,12 +6,21 @@
 #
 # Exit on first error, print all commands.
 set +ev
-for container in <%= dockerName %>_orderer.example.com <%= dockerName %>_ca.org1.example.com <%= dockerName %>_peer0.org1.example.com <%= dockerName %>_couchdb <%= dockerName %>_logspout; do
-  running=$(docker inspect -f '{{.State.Running}}' $container 2>/dev/null)
-  if [ "$?" != "0" ]; then 
+
+isRunning() {
+  running=$(docker inspect -f '{{.State.Running}}' $1 2>/dev/null)
+  if [ "$?" != "0" ]; then
     exit 1
-  elif [ "${running}" != "true" ]; then 
+  elif [ "${running}" != "true" ]; then
     exit 1
   fi
-done
+}
+
+if [ "$#" -gt 0 ]; then
+    isRunning <%= dockerName %>-peer0.org1.example.com-$1
+else
+  for container in <%= dockerName %>_orderer.example.com <%= dockerName %>_ca.org1.example.com <%= dockerName %>_peer0.org1.example.com <%= dockerName %>_couchdb <%= dockerName %>_logspout; do
+    isRunning $container;
+  done
+fi
 exit 0
