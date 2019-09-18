dc05dcefe734311ad9d38a9ba7ab8b07eed82f6c
Erin <Erin.Hughes@ibm.com>
2019-09-18 16:37:31 +0100

    updating fabric images to 1.4.3
    Signed-off-by: Erin <Erin.Hughes@ibm.com>

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -26,7 +26,7 @@ else
 fi
 
 pushd tmp
-curl -sSL http://bit.ly/2ysbOFE | bash -s 1.4.2
+curl -sSL http://bit.ly/2ysbOFE | bash -s 1.4.3
 pushd fabric-samples/basic-network
 export FABRIC_DIR="$(pwd)"
 ./start.sh
