691bed176dc30a9447bb6c45ae69f73d18e5bb7e
Simon Stone <sstone1@uk.ibm.com>
2019-12-30 09:57:43 +0000

    Set chaincode name prefix, add kill_chaincode scripts (resolves #212)
    Signed-off-by: Simon Stone <sstone1@uk.ibm.com>

--- generators/network/templates/kill_chaincode.sh
+++ generators/network/templates/kill_chaincode.sh
@@ -6,4 +6,12 @@
 #
 # Exit on first error, print all commands.
 set +ev
+if [ "$#" -ne 2 ]; then
+    exit 1
+fi
+CHAINCODE_NAME=$1
+CHAINCODE_VERSION=$2
+for CONTAINER in $(docker ps -f name="^<%= dockerName %>" -f label=org.hyperledger.fabric.chaincode.id.name="${CHAINCODE_NAME}" -f label=org.hyperledger.fabric.chaincode.id.version="${CHAINCODE_VERSION}" -q -a); do
+    docker rm -f ${CONTAINER}
+done
 exit 0
