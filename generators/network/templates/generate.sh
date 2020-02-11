6fbe017470f7ba7cc813a61183a0fa33bfeb6f31
Simon Stone <sstone1@uk.ibm.com>
2020-02-11 13:55:25 +0000

    Always fix permissions, even if playbook execution fails (contributes to #248)
    Signed-off-by: Simon Stone <sstone1@uk.ibm.com>

--- generators/network/templates/generate.sh
+++ generators/network/templates/generate.sh
@@ -6,6 +6,8 @@
 #
 # Exit on first error, print all commands.
 set -ev
+fix_permissions () {
+    docker run --rm -v "$PWD":/network ibmblockchain/vscode-prereqs:0.0.10 chown -R $(id -u):$(id -g) /network
+}
+trap fix_permissions EXIT
 docker run --rm -v "$PWD":/network -v /var/run/docker.sock:/var/run/docker.sock --network host ibmblockchain/vscode-prereqs:0.0.10 ansible-playbook /network/playbook.yml
-docker run --rm -v "$PWD":/network ibmblockchain/vscode-prereqs:0.0.10 chown -R $(id -u):$(id -g) /network
-exit 0
