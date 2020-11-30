09d1d8a1762135f7abefa9b741b42a678f04ad24
Jake Turner <10009752+Jakeeyturner@users.noreply.github.com>
2020-11-30 10:25:02 +0000

    Updated TypeScript types
    Signed-off-by: Jake Turner <10009752+Jakeeyturner@users.noreply.github.com>

--- generators/contract/templates/v2/default/typescript/src/my-contract.ts
+++ generators/contract/templates/v2/default/typescript/src/my-contract.ts
@@ -11,49 +11,49 @@ export class <%= assetPascalCase %>Contract extends Contract {
     @Transaction(false)
     @Returns('boolean')
     public async <%= assetCamelCase %>Exists(ctx: Context, <%= assetCamelCase %>Id: string): Promise<boolean> {
-        const buffer = await ctx.stub.getState(<%= assetCamelCase %>Id);
-        return (!!buffer && buffer.length > 0);
+        const data: Uint8Array = await ctx.stub.getState(<%= assetCamelCase %>Id);
+        return (!!data && data.length > 0);
     }
 
     @Transaction()
     public async create<%= assetPascalCase %>(ctx: Context, <%= assetCamelCase %>Id: string, value: string): Promise<void> {
-        const exists = await this.<%= assetCamelCase %>Exists(ctx, <%= assetCamelCase %>Id);
+        const exists: boolean = await this.<%= assetCamelCase %>Exists(ctx, <%= assetCamelCase %>Id);
         if (exists) {
             throw new Error(`The <%= assetSpaceSeparator %> ${<%= assetCamelCase %>Id} already exists`);
         }
-        const <%= assetCamelCase %> = new <%= assetPascalCase %>();
+        const <%= assetCamelCase %>: <%= assetPascalCase %> = new <%= assetPascalCase %>();
         <%= assetCamelCase %>.value = value;
-        const buffer = Buffer.from(JSON.stringify(<%= assetCamelCase %>));
+        const buffer: Buffer = Buffer.from(JSON.stringify(<%= assetCamelCase %>));
         await ctx.stub.putState(<%= assetCamelCase %>Id, buffer);
     }
 
     @Transaction(false)
     @Returns('<%= assetPascalCase %>')
     public async read<%= assetPascalCase %>(ctx: Context, <%= assetCamelCase %>Id: string): Promise<<%= assetPascalCase %>> {
-        const exists = await this.<%= assetCamelCase %>Exists(ctx, <%= assetCamelCase %>Id);
+        const exists: boolean = await this.<%= assetCamelCase %>Exists(ctx, <%= assetCamelCase %>Id);
         if (!exists) {
             throw new Error(`The <%= assetSpaceSeparator %> ${<%= assetCamelCase %>Id} does not exist`);
         }
-        const buffer = await ctx.stub.getState(<%= assetCamelCase %>Id);
-        const <%= assetCamelCase %> = JSON.parse(buffer.toString()) as <%= assetPascalCase %>;
+        const data: Uint8Array = await ctx.stub.getState(<%= assetCamelCase %>Id);
+        const <%= assetCamelCase %>: <%= assetPascalCase %> = JSON.parse(data.toString()) as <%= assetPascalCase %>;
         return <%= assetCamelCase %>;
     }
 
     @Transaction()
     public async update<%= assetPascalCase %>(ctx: Context, <%= assetCamelCase %>Id: string, newValue: string): Promise<void> {
-        const exists = await this.<%= assetCamelCase %>Exists(ctx, <%= assetCamelCase %>Id);
+        const exists: boolean = await this.<%= assetCamelCase %>Exists(ctx, <%= assetCamelCase %>Id);
         if (!exists) {
             throw new Error(`The <%= assetSpaceSeparator %> ${<%= assetCamelCase %>Id} does not exist`);
         }
-        const <%= assetCamelCase %> = new <%= assetPascalCase %>();
+        const <%= assetCamelCase %>: <%= assetPascalCase %> = new <%= assetPascalCase %>();
         <%= assetCamelCase %>.value = newValue;
-        const buffer = Buffer.from(JSON.stringify(<%= assetCamelCase %>));
+        const buffer: Buffer = Buffer.from(JSON.stringify(<%= assetCamelCase %>));
         await ctx.stub.putState(<%= assetCamelCase %>Id, buffer);
     }
 
     @Transaction()
     public async delete<%= assetPascalCase %>(ctx: Context, <%= assetCamelCase %>Id: string): Promise<void> {
-        const exists = await this.<%= assetCamelCase %>Exists(ctx, <%= assetCamelCase %>Id);
+        const exists: boolean = await this.<%= assetCamelCase %>Exists(ctx, <%= assetCamelCase %>Id);
         if (!exists) {
             throw new Error(`The <%= assetSpaceSeparator %> ${<%= assetCamelCase %>Id} does not exist`);
         }
