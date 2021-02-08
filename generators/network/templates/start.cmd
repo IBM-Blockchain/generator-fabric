00828be63fc9a7bc2e1a86aab713891fa9471de9
Leonor Quintais <lquintai@uk.ibm.com>
2021-02-08 13:45:34 +0000

    Update microfab to v0.0.10 and audit fix
    Signed-off-by: Leonor Quintais <lquintai@uk.ibm.com>

--- generators/network/templates/start.cmd
+++ generators/network/templates/start.cmd
@@ -16,7 +16,7 @@ IF DEFINED CONTAINER (
     )
 ) ELSE (
     SET MICROFAB_CONFIG=<%-microfabConfig%>
-    docker run -e MICROFAB_CONFIG --label fabric-environment-name="<%= name %> Microfab" -d -p <%-port%>:<%-port%> ibmcom/ibp-microfab:0.0.8
+    docker run -e MICROFAB_CONFIG --label fabric-environment-name="<%= name %> Microfab" -d -p <%-port%>:<%-port%> ibmcom/ibp-microfab:0.0.10
 )
 
 
