b9c6602505f4d2976877781e7431d29fba757f77
Jake <10009752+Jakeeyturner@users.noreply.github.com>
2021-02-18 13:30:23 +0000

    Allow custom start image (#406)
    Signed-off-by: Jake Turner <10009752+Jakeeyturner@users.noreply.github.com>

--- generators/network/templates/start.cmd
+++ generators/network/templates/start.cmd
@@ -6,6 +6,14 @@ rem SPDX-License-Identifier: Apache-2.0
 rem
 setlocal enabledelayedexpansion
 
+SET CUSTOM_IMAGE=%1
+if DEFINED CUSTOM_IMAGE (
+    SET START_IMAGE=%CUSTOM_IMAGE%
+) ELSE (
+    SET START_IMAGE="ibmcom/ibp-microfab:0.0.11"
+)
+
+echo "Using image: %START_IMAGE%"
 
 FOR /F "usebackq tokens=*" %%g IN (`docker ps -f label^=fabric-environment-name^="<%= name %> Microfab" -q -a`) do (SET CONTAINER=%%g)
 
@@ -16,7 +24,7 @@ IF DEFINED CONTAINER (
     )
 ) ELSE (
     SET MICROFAB_CONFIG=<%-microfabConfig%>
-    docker run -e MICROFAB_CONFIG --label fabric-environment-name="<%= name %> Microfab" -d -p <%-port%>:<%-port%> ibmcom/ibp-microfab:0.0.11
+    docker run -e MICROFAB_CONFIG --label fabric-environment-name="<%= name %> Microfab" -d -p <%-port%>:<%-port%> %START_IMAGE%
 )
 
 
